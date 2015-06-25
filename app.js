6535339e3b24f89f8018e33f27b7cba7343de145
azaridi <Dimitris.AZARIAS@ext.ec.europa.eu>
2015-06-25 09:31:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+
+// set our port
+var port = process.env.PORT || 80;
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+require('./routes')(app); // configure our routes
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+exports = module.exports = app;
