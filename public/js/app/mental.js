7fdafb3d0dc6701993f62157f66b5a48d355d8cb
azaridi <Dimitris.AZARIAS@ext.ec.europa.eu>
2015-09-16 15:36:50 +0200

    i18n

--- public/js/app/mental.js
+++ public/js/app/mental.js
@@ -3,20 +3,82 @@ var mentalApp = angular.module('mentalApp', ['ngRoute', 'ui.bootstrap', 'pascalp
 //ANGULAR-TRANSLATE - TRANSLATION (i18n) TABLES
 mentalApp.config(function ($translateProvider) {
 	  //ENGLISH TRANSLATIONs
+	  $translateProvider.translations('gr', {
+		  SITE_HEADING : 'Mέντaλ Α.Ε',
+		  SITE_SUBHEADER : 'Ολοκληρωμένες Συμβουλευτικές Υπηρεσίες',
+		  LANDING_MORE : 'Περισσότερα',
+
+		  NAV_COMPANY : 'Εταιρεία',
+		  NAV_SERVICES : 'Υπηρεσίες',
+		  NAV_TESTIMONIALS : 'Οι πελάτες μας',
+		  NAV_ARTICLES : 'Αρθρα',
+		  NAV_COMM : 'Επικοινωνία',
+		  
+		  HEADER_COMPANY : 'Η Εταιρεία μας',
+		  HEADER_SERVICES : 'Οι Υπηρεσίες μας',
+		  HEADER_TESTIMONIALS : 'Οι πελάτες μας',
+		  HEADER_ARTICLES : 'Αρθρογραφία',
+		  HEADER_COMM : 'Επικοινωνήστε μαζί μας',
+
+	  });
+
+	  //ENGLISH TRANSLATIONs
 	  $translateProvider.translations('en', {
-		  SITE_HEADER : 'EXPORT CREDIT DATABASE',
+		  SITE_HEADING : 'Mental S.A',
+		  SITE_SUBHEADER : 'Complete Accounting Solutions',
+		  LANDING_MORE : 'More',
+		  
+		  NAV_COMPANY : 'Company',
+		  NAV_SERVICES : 'Services',
+		  NAV_TESTIMONIALS : 'Our Customers',
+		  NAV_ARTICLES : 'Articles',
+		  NAV_COMM : 'Communication',
+
+		  HEADER_COMPANY : 'Our Company',
+		  HEADER_SERVICES : 'Our Services',
+		  HEADER_TESTIMONIALS : 'Our Customers',
+		  HEADER_ARTICLES : 'Articles',
+		  HEADER_COMM : 'Contact Us',
 	  });
 
 	  //FRENCH TRANSLATIONs
 	  $translateProvider.translations('fr', {
-		  SITE_HEADER : 'EXPORT CREDIT DATABASE',
+		  SITE_HEADING : 'Mental S.A',
+		  SITE_SUBHEADER : 'Solutions Complètes de Comptabilité',
+		  LANDING_MORE : 'Plus',
+		  
+		  NAV_COMPANY : 'Entreprise',
+		  NAV_SERVICES : 'Services',
+		  NAV_TESTIMONIALS : 'Nos clients',
+		  NAV_ARTICLES : 'Articles',
+		  NAV_COMM : 'Communication',
+
+		  HEADER_COMPANY : 'Notre Entreprise',
+		  HEADER_SERVICES : 'Nos Services',
+		  HEADER_TESTIMONIALS : 'Nos clients',
+		  HEADER_ARTICLES : 'Articles',
+		  HEADER_COMM : 'Communication',
 	  });
 	  
 	  //GERMAN TRANSLATIONs
 	  $translateProvider.translations('de', {
-		  SITE_HEADER : 'EXPORT CREDIT DATABASE',
+		  SITE_HEADING : 'Mental S.A',
+		  SITE_SUBHEADER : 'Komplette Lösungen für das Rechnungswesen',
+		  LANDING_MORE : 'Mehr',
+		  
+		  NAV_COMPANY : 'Firma',
+		  NAV_SERVICES : 'Dienstleistungen',
+		  NAV_TESTIMONIALS : 'Unsere Kunden',
+		  NAV_ARTICLES : 'Gegenstände',
+		  NAV_COMM : 'Kommunikation',
+
+		  HEADER_COMPANY : 'Firma',
+		  HEADER_SERVICES : 'Dienstleistungen',
+		  HEADER_TESTIMONIALS : 'Unsere Kunden',
+		  HEADER_ARTICLES : 'Gegenstände',
+		  HEADER_COMM : 'Kommunikation',
 	  });
 	  
 	  //DEFAULT LANGUAGE
-	  $translateProvider.preferredLanguage('en');
+	  $translateProvider.preferredLanguage('gr');
 });
