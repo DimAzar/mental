d76cf4b07366a58319827f8544431246d268d12b
azaridi <Dimitris.AZARIAS@ext.ec.europa.eu>
2016-02-12 17:39:32 +0100

    res.sendFile fix

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 module.exports = function(app) {
   app.get('/mental', function(req, res) {
-    res.sendfile('./public/views/portal.html'); 
+	  res.sendFile('public/views/portal.html', { root:__dirname+'/..'}); 
   });
   app.get('/mental/group', function(req, res) {
-	    res.sendfile('public/views/index.html');
+	  res.sendFile('public/views/index.html', { root:__dirname+'/..'}); 
+  });
+  app.get('/mental/group/latest', function(req, res) {
+	  res.sendFile('public/views/partials/blog.html', { root:__dirname+'/..'});  
   });
   app.get('/mental/group/single-post', function(req, res) {
-	    res.sendfile('public/views/partials/single-post.html'); 
-  });
-
-  app.get('/mental/group/latest', function(req, res) {
-	    res.sendfile('public/views/partials/blog.html'); 
+	  res.sendFile('public/views/partials/single-post.html', { root:__dirname+'/..'}); 
   });
   app.get('/mental/group/templates/:name', function(req, res) {
-	    res.sendfile('public/views/templates/'+req.params.name); // load our public/index.html file
-	  });
+	  res.sendFile('public/views/templates/'+req.params.name , { root:__dirname+'/..'}); 
+  });
 };
